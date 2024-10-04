458845ccc943e665f0fec95b94c20b63d1f9512a
Ravi Malik <rm.rvmalik@gmail.com>
2024-10-04 07:09:09 +0530

    Initial commit

---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,16 @@
+package Oct.ex_03102024;
+
+public class Lab002
+{
+    public static void main(String[] args) {
+        System.out.println("10 * 1 = 10");
+        System.out.println("10 * 2 = 20");
+        System.out.println("10 * 3 = 30");
+        System.out.println("10 * 4 = 40");
+        System.out.println("10 * 5 = 50");
+        System.out.println("10 * 6 = 60");
+        System.out.println("10 * 10 = 100");
+
+
+    }
+}
