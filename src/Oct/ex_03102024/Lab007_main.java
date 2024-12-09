ddb99df622e4bacc8c4baba09de186ca8834aeb8
Ravi Malik <rm.rvmalik@gmail.com>
2024-12-09 14:38:47 +0530

    Git commit 9 dec

--- src/Oct/ex_03102024/Lab007_main.java
+++ src/Oct/ex_03102024/Lab007_main.java
@@ -2,6 +2,6 @@ package Oct.ex_03102024;
 
 public class Lab007_main {
     public static void main(String[] args) {
-        System.out.println("Hello Worl d!!!");
+        System.out.println("Hello World!!!");
     }
 }
