8a1925d5cf1ead069f6cb0179a92997cb1d82542
Ravi Malik <rm.rvmalik@gmail.com>
2024-12-09 22:11:12 +0530

    Git commit for variable Lab010

---
+++ src/Oct/ex_08102024/Lab010_variable.java
@@ -0,0 +1,12 @@
+package Oct.ex_08102024;
+
+public class Lab010_variable {
+    public static void main(String[] args) {
+        int age =65;
+        System.out.println(age);
+        //int age =78;  reassigning the value of variable is not possible
+        age=66;
+        System.out.println(age);
+
+    }
+}
