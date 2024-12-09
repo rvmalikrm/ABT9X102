7236c693aba74bc954f5e805ef8220d1e1146c75
Ravi Malik <rm.rvmalik@gmail.com>
2024-12-09 14:34:56 +0530

    Git commit 9 dec

---
+++ src/Oct/ex_03102024/Lab003_mainmethod.java
@@ -0,0 +1,12 @@
+package Oct.ex_03102024;
+
+import java.net.StandardSocketOptions;
+
+public class Lab003_mainmethod
+{
+    public static void main(String[] ravi) {
+
+        System.out.println("Hello Java");
+
+    }
+}
