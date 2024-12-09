9cfba5042b65ed63c6d2d78bbf3bb89dfdef7e7a
Ravi Malik <rm.rvmalik@gmail.com>
2024-12-09 22:54:11 +0530

    Git commit for variable boolean type 2

---
+++ src/Oct/ex_08102024/Lab014.java
@@ -0,0 +1,16 @@
+package Oct.ex_08102024;
+
+public class Lab014 {
+    public static void main(String[] args) {
+        boolean is_ravi_married = true;
+        boolean is_male= true;
+        boolean ravi_has_Audi = false;
+        //boolean are_you_india = No Idea;
+
+
+
+        System.out.println(is_male);
+        System.out.println(ravi_has_Audi);
+        System.out.println(is_ravi_married);
+    }
+}
