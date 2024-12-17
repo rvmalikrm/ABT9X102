24cfc66a31c0306697bc276ea55086c522068aba
Ravi Malik <rm.rvmalik@gmail.com>
2024-12-17 21:46:57 +0530

    Git commit for Lab 25 p1

--- src/Oct/ex_09102024/Lab024.java
+++ src/Oct/ex_09102024/Lab024.java
@@ -4,5 +4,6 @@ public class Lab024 {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.println("Hello World");
+        //System.out.printf("Hello World");
     }
 }
---
+++ src/Oct/ex_09102024/Lab025.java
@@ -0,0 +1,9 @@
+package Oct.ex_09102024;
+
+public class Lab025 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.printf("Your age is %s ", a);
+    }
+}
