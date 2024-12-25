aa7752949952d5ac2737f4d39e96a3231df3865d
Ravi Malik <rm.rvmalik@gmail.com>
2024-12-25 15:47:00 +0530

    Git commit for Practice001

---
+++ src/Question250Plus/ex25Dec/Practice001.java
@@ -0,0 +1,19 @@
+package Question250Plus.ex25Dec;
+
+import java.util.Scanner;
+
+public class Practice001 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the Name");
+        String str1= sc.nextLine();
+        System.out.println("Enter the age");
+        int age = sc.nextInt();
+        //String str1= sc.nextLine();
+        System.out.println(str1);
+        System.out.println(age);
+
+
+    }
+}
