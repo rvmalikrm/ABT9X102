3c6fe9ea4f437c7a059d2dc8125bc6c636cfd7de
Ravi Malik <rm.rvmalik@gmail.com>
2025-01-25 12:58:52 +0530

    Commit push 25 Jan 25 Reverse the String

---
+++ src/Oct/Task/task_by_reverse_string_preserve_whitespace.java
@@ -0,0 +1,17 @@
+package Oct.Task;
+
+public class task_by_reverse_string_preserve_whitespace {
+
+        public static void main(String[] args) {
+            String s="This is Dog";
+            int strlen=s.length();
+            String rs="";
+
+            System.out.println(strlen);
+            for(int i=strlen;i>0;i--)
+            {
+                System.out.print(s.charAt(i-1));
+            }
+        }
+    }
+
