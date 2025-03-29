7c51b2565c7b06568cab9a95a0d284eac88afab8
Ravi Malik <rm.rvmalik@gmail.com>
2025-03-29 22:08:59 +0530

    Commit for Palindrome program

--- src/Mar/Lab20Mar25_Inheritance.java
+++ src/Mar/Lab20Mar25_Inheritance.java
@@ -1,24 +1,24 @@
-package Mar;
-
-public class Lab20Mar25_Inheritance {
-    protected int count;
-
-    public  Lab20Mar25_Inheritance(){
-        count=count++;
-    }
-
-    void display(int count){
-        System.out.println("here is the value ot "+count);
-    }
-
-    public static void main(String[] args) {
-
-
-        Lab20Mar25_Inheritance obj =new Lab20Mar25_Inheritance(int count);
-
-        obj.count=10;
-        void decrement(20){
-            this.count=count--;
-        }
-    }
-}
+//package Mar;
+//
+//public class Lab20Mar25_Inheritance {
+//    protected int count;
+//
+//    public  Lab20Mar25_Inheritance(){
+//        count=count++;
+//    }
+//
+//    void display(int count){
+//        System.out.println("here is the value ot "+count);
+//    }
+//
+//    public static void main(String[] args) {
+//
+//
+//        Lab20Mar25_Inheritance obj =new Lab20Mar25_Inheritance(int count);
+//
+//        obj.count=10;
+//        void decrement(20){
+//            this.count=count--;
+//        }
+//    }
+//}
