0f2e07da5bfef4adb9217ad233f4ec09a25103a7
Ravi Malik <rm.rvmalik@gmail.com>
2025-04-23 22:47:39 +0530

    Commit for Abstraction

---
+++ src/Apr/Lab5Apr25_Abstraction.java
@@ -0,0 +1,33 @@
+package Apr;
+abstract class Base
+{
+    public void method1()
+    {
+        System.out.println("This is regular method");
+    }
+    abstract void method();
+
+}
+class Down extends Base
+{
+    public void method(){
+        System.out.println("This is Down regular method");
+    }
+}
+class Top extends Base
+{
+    public void method(){
+        System.out.println("This is Top regular method");
+    }
+}
+public class Lab5Apr25_Abstraction {
+    public static void main(String[] args) {
+        Base myDown=new Down();
+        Base myTop= new Top();
+        myTop.method();
+        myTop.method1();
+        myDown.method();
+        myDown.method1();
+
+    }
+}
