a0f0be97f5b0e3033511a0318e0ee14d2b81c8aa
Ravi Malik <rm.rvmalik@gmail.com>
2025-05-06 23:01:59 +0530

    First non repeat character in an String

--- src/May/Lab6may25_firstNonRepeativeChar.java
+++ src/May/Lab6may25_firstNonRepeativeChar.java
@@ -1,23 +1,30 @@
 package May;
-
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-
 public class Lab6may25_firstNonRepeativeChar {
     public static void main(String[] args) {
         String input ="automation";
-        char ch=firstNonRepeatChar(input);
-        System.out.println(ch);
+        Character ch=firstNonRepeatChar(input);
+        if(ch!='\0') {
+            System.out.println("Non repeative char "+ch);
+        }
+        else{
+            System.out.println("There is no non repeative character in given String");
+        }
     }
     public static char firstNonRepeatChar(String input)
     {
         char[] chararr=input.toCharArray();
-        Map<Character, Integer> mpfreq=new HashMap<>();
+        Map<Character, Integer> mpfreq=new LinkedHashMap<>();
         StringBuilder freq=new StringBuilder();
         for(char  ch:chararr){
-            mpfreq.put(ch,mpfreq.getOrDefault(ch,9)+1);
-            System.out.println(mpfreq);
-            return ch;
+            mpfreq.put(ch,mpfreq.getOrDefault(ch,0)+1);
+        }
+        // Find the first character with frequency 1
+        for (Map.Entry<Character, Integer> entry: mpfreq.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
         }
         return '\0';
     }
