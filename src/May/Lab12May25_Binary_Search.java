6b7642ce3de498759d47cc7da8762e1f70373f40
Ravi Malik <rm.rvmalik@gmail.com>
2025-05-14 07:15:34 +0530

    Binary search tree search the target number

--- src/May/Lab12May25_Binary_Search.java
+++ src/May/Lab12May25_Binary_Search.java
@@ -1,5 +1,4 @@
 package May;
-
 public class Lab12May25_Binary_Search {
     public static void main(String[] args) {
         int[] input= {1,2,3,5,7,8,10,13,16};
@@ -12,7 +11,6 @@ public class Lab12May25_Binary_Search {
             System.out.println(" The element is not found");
         }
     }
-
     public static int binarySearch(int[] arr, int target){
         int left =0, right= arr.length-1;
         while (left<=right)
