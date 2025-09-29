39c82f5efcd7b873e9045456ae554e9f04b353cc
Ravi Malik <rm.rvmalik@gmail.com>
2025-09-29 22:21:19 +0530

    Binary incomplete rotate

---
+++ src/Mar/Lab29Sept25_Palindrome.java
@@ -0,0 +1,22 @@
+package Mar;
+
+public class Lab29Sept25_Palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        boolean output = isPalindrom(str);
+        System.out.println(output);
+    }
+    public static boolean isPalindrom(String str){
+        str= str.toLowerCase();
+        int left= 0;
+        int right= str.length()-1;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return  false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
