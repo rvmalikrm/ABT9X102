933aae04ea4b6cba8c11a4a280cdd82b726dfca0
Ravi Malik <rm.rvmalik@gmail.com>
2025-10-01 07:25:44 +0530

    Lab01Oct25_ReverseString

---
+++ src/Oct2025/Lab01Oct25_ReverseString.java
@@ -0,0 +1,11 @@
+package Oct2025;
+
+public class Lab01Oct25_ReverseString {
+    public static void main(String[] args) {
+        String str= "Automation";
+        for(int i= str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+
+        }
+    }
+}
