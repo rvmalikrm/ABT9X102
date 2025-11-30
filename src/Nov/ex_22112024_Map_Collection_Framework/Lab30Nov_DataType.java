a639b2381c1ccc5425c0a4bbd43c428cb8db8e14
Ravi Malik <rm.rvmalik@gmail.com>
2025-11-30 09:56:26 +0530

    Lab30Nov_DataType

---
+++ src/Nov/ex_22112024_Map_Collection_Framework/Lab30Nov_DataType.java
@@ -0,0 +1,23 @@
+package Nov.ex_22112024_Map_Collection_Framework;
+
+public class Lab30Nov_DataType {
+    public static void main(String[] args) {
+        byte b=3;
+        short s= 34;
+        int i= 12345;
+        long l= 9568810724l;
+        float f= 12.3f;
+        double d= 45.6789d;
+        char c= 'A';
+        boolean bool= true;
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Integer value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + bool);
+
+    }
+}
